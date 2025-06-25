79da0fecc9ac1219767af961244335add69161bd
VivasvanVerma <vvofficial.yt@gmail.com>
2025-06-25 18:23:49 +0530

    tuple

---
+++ tuple.py
@@ -0,0 +1,12 @@
+tuple1 = ('tuple', False, 3, 2, 1)
+print(tuple1)
+
+tuplex = (4, 6, 2, 8, 3 ,1)
+print(tuplex + (9,))
+
+tupley = (50, 60, 70, 50, 80, 50)
+print(tupley.count(50))
+
+tuplea = (2, 3, 4, 5, 7, 3, 5, 3,1,5,6, 7)
+print(tuplea[3:5])
+print(tuplea[:6])
